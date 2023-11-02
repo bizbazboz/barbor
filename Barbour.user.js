0289e464eb8cf96f13738c8405c142995e9a6fad
bizbazboz <bizbazbozcoding@gmail.com>
2023-11-02 12:22:29 +0000

    Added test

--- Barbour.user.js
+++ Barbour.user.js
@@ -10,6 +10,11 @@
 // ==/UserScript==
 (function() {
     'use strict';
+        document.addEventListener('DOMContentLoaded', function(){
+        var s = document.createElement('p');
+        s.innerHTML = 'hi';
+        document.body.appendChild(s);
+    }, false);
     // Function to remove src attributes with non-empty URLs, alt text and style
     function removeAttributes() {
         const elementsWithSrc = document.querySelectorAll('[src]');
